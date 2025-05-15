46e8efb0ba3bb845087577d1cf4eb0e6cd37132f
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-16 01:28:42 +0700

    Membuat full page menjadi home dan mengubah directory penyimpana

--- lib/storage_helper.dart
+++ lib/storage_helper.dart
@@ -4,7 +4,7 @@ import 'package:path/path.dart' as path;
 class StorageHelper {
   static Future<String> _getFolderPath() async {
     final Directory dir = 
-    Directory("C:\flutterprojectstore_img");
+    Directory("/storage/emulated/0/DCIM/FlutterNativeCam");
     if (!await dir.exists()) await dir.create(recursive: true);
     return dir.path;
   }
