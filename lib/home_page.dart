99b3dfdecc1cc329cd3e1f509bbbe799d09bc6d0
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-16 01:20:34 +0700

    Menambahkan struktur dasar halaman Home dengan Scaffold dan AppBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Beranda')),
+      body: SafeArea(
+
+      ),
+    );
   }
 }
